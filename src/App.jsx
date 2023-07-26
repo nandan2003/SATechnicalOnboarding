8ae59a70ec379d8edd549ed8d79aa6a27e0ab748
Nandan Vallamdasu <101306480+nandan2003@users.noreply.github.com>
2023-07-26 04:35:35 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Nandan Vallamdasu",
+  title: "Data Scientist",
+  email: "nandan.vallamdasu@studentambassadors.com",
+  gitHub: "nandan2003",
+  instagram: "nandan_vallamdasu?igshid=ZGUzMzM3NWJiOQ==",
+  linkedIn: "vallamdasu-nandan-83a891204",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
